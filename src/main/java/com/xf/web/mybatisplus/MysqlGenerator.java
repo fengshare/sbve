61fdd52aaca75e361bdf376be767fd03cf5f2096
xiefeng <m15779065925@163.com>
2020-07-10 21:12:22 +0800

    MysqlGenerator

---
+++ src/main/java/com/xf/web/mybatisplus/MysqlGenerator.java
@@ -0,0 +1,115 @@
+package com.xf.web.mybatisplus;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * mybatis plus mysql 代码生成器
+ * </p>
+ */
+public class MysqlGenerator {
+
+    /**
+     * RUN THIS
+     */
+    public static void main(String[] args) {
+        String projectPath = "sbve";
+
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("xf");
+        gc.setOpen(true);
+        //service 命名方式
+        gc.setServiceName("%sService");
+        //service impl 命名方式
+        gc.setServiceImplName("%sServiceImpl");
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(true);
+        // XML columList
+        gc.setBaseColumnList(false);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://39.108.160.190:3360/sbve?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
+//        dsc.setUrl("jdbc:mysql://10.168.1.166:3306/rx_swis_bedside?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("rxdevrw");
+        dsc.setPassword("wgKk9lD8ffOEaVbt");
+//        dsc.setUsername("root");
+//        dsc.setPassword("qwertyuiop");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.rixiangtek.bedside.screen");
+        pc.setEntity("entity");
+        pc.setMapper("mapper");
+//        pc.setService("service");
+//        pc.setServiceImpl("service.impl");
+//        pc.setController("web.controller.rest");
+        mpg.setPackageInfo(pc);
+
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return "swis-bedside-screen/swis-bedside-screen-service" + "/src/main/resources/mapper/"
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+        mpg.setTemplate(new TemplateConfig().setXml(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        // 设置逻辑删除键
+//        strategy.setLogicDeleteFieldName("deleted");
+        strategy.setInclude("t_recharge_log");
+        //strategy.setSuperEntityColumns("id");
+        // 去掉表名的前缀
+        strategy.setTablePrefix("t_");
+        //驼峰转连字符
+        strategy.setControllerMappingHyphenStyle(true);
+        //生成的Controller类自带@RestController注解
+        strategy.setRestControllerStyle(true);
+        mpg.setStrategy(strategy);
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
